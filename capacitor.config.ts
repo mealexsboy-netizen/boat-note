0879ae1c7972743241ff13ece96983f6d30eb4d0
mealexsboy-netizen <mealexsboy@gmail.com>
2026-04-15 01:30:10 +0500

    Change webDir from 'public' to 'out'

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.touchsizx.grom',
   appName: 'note-boat',
-  webDir: 'public'
+  webDir: 'out'
 };
 
 export default config;
